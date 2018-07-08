b21e5f5cde798d67150e0372280fc9ce35bb907e
XUHO3 <howells.xu@oocl.com>
2018-07-08 09:59:23 +0800

    init code and push tasking

---
+++ src/best-charge.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {items} = require('./items');
+const {promotions} = require('./promotions');
+
+
+function bestCharge(selectedItems) {
+  return /*TODO*/;
+}
+
+module.exports={
+  bestCharge
+ }
